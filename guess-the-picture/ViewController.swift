96dbc2f5ed59a7eb575ea5ac8db67620f9515d26
Liu Teng Hou <liutenghou@gmail.com>
2018-05-27 16:15:17 -0700

    first commit

---
+++ guess-the-picture/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  guess-the-picture
+//
+//  Created by Liu Teng Hou on 5/27/18.
+//  Copyright © 2018 wewantcookies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ guess-the-pictureTests/guess_the_pictureTests.swift
@@ -0,0 +1,36 @@
+//
+//  guess_the_pictureTests.swift
+//  guess-the-pictureTests
+//
+//  Created by Liu Teng Hou on 5/27/18.
+//  Copyright © 2018 wewantcookies. All rights reserved.
+//
+
+import XCTest
+@testable import guess_the_picture
+
+class guess_the_pictureTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
